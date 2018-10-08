712bebeb63d84cc562b77c4c69bdd56bba04e0de
epiphania <Vincente@lakeland.edu>
2018-10-08 12:41:16 -0500

    Update linearRegCostFunction.m
    longer version:
    
    hy = X*theta;
    hyError = hy - y;
    sumofSquaredError = sum(hError .^ 2);
    regLeft = (1/(2 * m)) * sumofSquaredError;
    
    thetaWithoutBias = theta(2:end);
    sumofSquaredTheta = sum(thetaWithoutBias .^ 2);
    regRight = (lambda / (2 * m)) * sumofSquaredTheta;
    
    J = regLeft + regRight;
    
    grad = (1/m) * X' * hyError;
    grad(2:end) += (lambda / m) * thetaWithoutBias;

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,7 +20,12 @@ grad = zeros(size(theta));
 %
 
 
+J = (1/(2*m))*sum(power((X*theta - y),2))+ (lambda/(2*m)) * sum(power(theta(2:end),2));
 
+G = (lambda/m) .* theta;
+G(1) = 0; % this is always 0
+
+grad = ((1/m) .* X' * (X*theta - y)) + G;
 
 
 
