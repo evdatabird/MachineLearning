1541f03234412c4da6db68470470fee585161fbc
epiphania <Vincente@lakeland.edu>
2018-10-17 11:16:26 -0500

    Update estimateGaussian.m

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -21,11 +21,13 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-%mu = 1/m * sum(X)
-%sigma2 = 1/m * sum((X - repmat(mu, m, 1)).^2);
+mu = 1/m * sum(X);
 
-mu = mean(X)
-sigma2 = var(X)
+sigma2 = 1/m * sum((X - repmat(mu, m, 1)).^2);
+
+
+%mu = mean(X)
+%sigma2 = var(X)
 
 
 
