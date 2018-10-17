6417a03548b7b169cc801b0d364c02ec8fcfda6c
epiphania <Vincente@lakeland.edu>
2018-10-17 11:35:01 -0500

    Update cofiCostFunc.m

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,7 +40,17 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+%J = (1/2).*sum(sum(((X*Theta').*R-Y.*R).^2))+(lambda./2.*sum(sum(Theta.^2)))+(lambda./2.*sum(sum(X.^2)));        
+% Only predict rating X*Theta' if user has rated (i.e. R=1)
 
+%X_grad = (((X*Theta').*R*Theta-Y.*R*Theta)+lambda.*X);
+%Theta_grad = ((X'*((X*Theta').*R)-X'*(Y.*R)))'+lambda.*Theta;
+
+J_temp = (X * Theta' - Y).^2;
+J = sum(sum(J_temp(R == 1)))/2 + lambda/2 .* sum(sum(Theta.^2)) + lambda/2 .* sum(sum(X.^2));
+
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda.*X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda.*Theta;
 
 
 
